7662f5e14191608cf49ba0ff8ac49fda7a63c3bf
leo-jansen <janseleo@gmail.com>
2023-09-18 22:40:12 -0300

    feat: atualização de estilo da pokedex

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Trilha JS Developer - Pokedex
+
+Projeto do curso de `Construindo uma Pokédex com JavaScript` do bootcamp ``Santander Bootcamp 2023 - Fullstack Java+Angular``
--- assets/js/main.js
+++ assets/js/main.js
@@ -7,19 +7,22 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
+        <div class="card border-secondary text-center m-2">
+            <h5 class="card-header">#${pokemon.number}.${pokemon.name}</h5>
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${pokemon.photo}" alt="${pokemon.name}" class="img-fluid rounded-start">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h6 class="card-title">Types</h6>
+                        <ul class="list-group list-group-flush">
+                            ${pokemon.types.map(type => `<li class="list-group-item ${type}">${type}</li>`).join('')}
+                        </ul>
+                    </div>
+                </div>
             </div>
-        </li>
+        </div>
     `
 }
 
